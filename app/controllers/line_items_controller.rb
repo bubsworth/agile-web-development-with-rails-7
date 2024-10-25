ce987aaa6d4e4db477fa6f047f74de436577c57e
bubsworth <bubbard96@hotmail.com>
2024-10-25 16:04:41 +0100

    Iteration E2: Handling Errors

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,11 +28,15 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to cart_url(@line_item.cart),
+          notice: "Line item was successfully created." }
+        format.json { render :show,
+          status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :new,
+          status: :unprocessable_entity }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -68,6 +72,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
