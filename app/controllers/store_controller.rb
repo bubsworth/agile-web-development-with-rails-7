d486530112e9768d5e029d425f8281140435d3b7
bubsworth <bubbard96@hotmail.com>
2024-11-08 16:04:24 +0000

    Iteration F1: Moving the Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order(:title)
+  end
+
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart.add_product(product)
   end
 end
